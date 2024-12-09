7287e7dd76cc306b3a8251336f0c1f66ddd6c835
Jag Bains <git@jagbains.org>
2024-12-09 12:17:42 +0000

    Thar be dragons in here

--- day9/2.py
+++ day9/2.py
@@ -4,6 +4,11 @@ f = open("input.txt")
 #This gets me to a file layout, one block per array index
 #Iterate from the beginning and end, moving blocks from end to spaces from beginning, until the two cross over
 
+#Bleurgh, I don't think my data structure is great for part 2
+#From end of array, find a file id, iterate till the end of that file to find size, repeat until fid=0
+#Then iterate from beginning of array with a window of that size, looking for all Nones, if one is found, write the file id to those positions, and overwrite the files original positions with None
+#Then do the math
+
 blocks=[]
 space=0 #0=file,1=space
 fid=0
@@ -22,20 +27,39 @@ for char in f.readline().strip():
         for i in range(size):
             blocks.append(None)
             space=0
-#print(blocks)
+print(blocks)
 #Compactify the filesystem
-lindex=0
-rindex=len(blocks)-1
-while (rindex>lindex):
-#    print (lindex,rindex)
-    if blocks[lindex] is not  None:
-        lindex+=1
-        continue
-    elif blocks[rindex] is not None:
-        blocks[lindex]=blocks[rindex]
-        blocks[rindex]=None
-    rindex-=1
 
+rindex=len(blocks)-1
+#print (len(blocks),rindex,fid, blocks[-1])
+while (fid>0):
+    fid-=1
+    filesize=0
+#    print (len(blocks),rindex,fid, blocks[rindex])
+    while blocks[rindex] != fid:
+#        print(fid,rindex)
+        rindex-=1
+    while(blocks[rindex]==fid):
+        filesize+=1
+        rindex-=1
+#    print(rindex,fid,filesize)
+    found=0
+    lindex=0
+    while(found==0 and (lindex+filesize)<len(blocks) and lindex+filesize-1<rindex+1):
+        window=blocks[lindex:lindex+filesize]
+#        print(window)
+        if (window.count(None) == len(window)):
+#            print("found a space!",lindex)
+            #it fits!
+            found=1
+            for i in range(len(window)):
+                blocks[lindex+i]=fid
+                blocks[rindex+i+1]=None
+#            print(blocks)
+        else:
+            lindex+=1
+
+print(blocks)
 #Do the math
 total=0
 for i in range(len(blocks)):

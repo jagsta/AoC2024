992358677b71654a82a74ccc380cce61f8e90228
Jag Bains <git@jagbains.org>
2024-12-11 11:54:06 +0000

    This is still going to take till the heat death of the universe

--- day11/2.py
+++ day11/2.py
@@ -1,37 +1,34 @@
 from timeit import default_timer as timer
-f = open("example.txt")
+f = open("input.txt")
 
 
 def process_number(number,index):
-    if number==0:
+    if number=="0":
 #        print ("found 0")
-        stones[index]=1
-    elif len(str(number)) % 2 ==0:
-        if len(str(number))==2:
-           num1=int(str(number)[0])
-           num2=int(str(number)[1])
-        else:
-            num1 = int(str(number)[:(int(len(str(number))/2))])
-            num2 = int(str(number)[int(len(str(number))/2):])
+        stones[index]="1"
+    elif len(number) % 2 ==0:
+        j=int(len(number)/2)
+        num1 = number[:j]
+        num2 = number[j:]
 #        print ("found even number of digits",num1,num2)
 #       Do we need to preserve order here? I think not, it doesn't affect the rules which are atomic
         stones[index] = num1
         stones.append(num2)
     else:
-        number*=2024
-        stones[index]=number
+        number=int(number)*2024
+        stones[index]=str(number)
 
 
 
 stones=[]
 for number in f.readline().split():
-    stones.append(int(number.strip()))
+    stones.append(number.strip())
 #    print (stones)
 
 blinks=1
 while (blinks<76):
     start=timer()
-    for i in reversed(range(len(stones))):
+    for i in range(len(stones)):
 #        print("processing ",stones[i], i)
         process_number(stones[i],i)
     end=timer()

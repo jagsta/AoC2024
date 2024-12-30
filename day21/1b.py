7ea7c85612256eca196f96c7b57e582a898384c8
Jag Bains <jagb@Jags-MacBook-Pro.local>
2024-12-30 11:51:51 +0000

    changing all the nodes in the graph to strings, gah

--- day21/1b.py
+++ day21/1b.py
@@ -9,107 +9,57 @@ if len(sys.argv)>1:
 
 f=open(file)
 
-def mapkeypad(code):
-    # start at A, avoid 0,3 (if xdif is neg, move y first, otherwise x first)
-    # Need to press A after each key
-    seq=""
-    lastx=keymap['A'][0]
-    lasty=keymap['A'][1]
-    for key in code:
-        nextx=keymap[key][0]
-        nexty=keymap[key][1]
-        xdif=lastx-nextx
-        ydif=lasty-nexty
-        if ydif<0:
-            yc="v"
-        else:
-            yc="^"
-        if xdif<0:
-            xc=">"
-            for i in range(abs(xdif),0,-1):
-                seq+=xc
-            for i in range(abs(ydif),0,-1):
-                seq+=yc
-        else:
-            xc="<"
-            for i in range(abs(ydif),0,-1):
-                seq+=yc
-            for i in range(abs(xdif),0,-1):
-                seq+=xc
-        lastx=nextx
-        lasty=nexty
-        seq+="A"
-    return seq
-
-def mapdirpad(code):
-    # start at A, avoid 0,0 (if xdif is neg, move y first, otherwise x first)
-    # Need to press A after each key
-    seq=""
-    lastx=dirmap['A'][0]
-    lasty=dirmap['A'][1]
-    for key in code:
-        nextx=dirmap[key][0]
-        nexty=dirmap[key][1]
-        xdif=lastx-nextx
-        ydif=lasty-nexty
-        if ydif<0:
-            yc="v"
-        else:
-            yc="^"
-        if xdif<0:
-            xc=">"
-            for i in range(abs(xdif),0,-1):
-                seq+=xc
-            for i in range(abs(ydif),0,-1):
-                seq+=yc
-        else:
-            xc="<"
-            for i in range(abs(ydif),0,-1):
-                seq+=yc
-            for i in range(abs(xdif),0,-1):
-                seq+=xc
-        lastx=nextx
-        lasty=nexty
-        seq+="A"
-    return seq
+def getkeypaths(a,b,paths):
+    if len(paths)>0:
+        for path in paths:
+            for p in keypaths[a][b]:
+                paths.add(path.append(p))
+    else:
+        for p in keypaths[a][b]:
+            paths.add(p)
+    return paths
 
 
+#Build graphs of keypads, directional so we can capture the directional moves between keys, then compute all shortest paths for all pairs of keys and store in a dict of dicts
 keys=nx.DiGraph()
 dirs=nx.DiGraph()
 #up
-keys.add_edges_from([("A",3),(0,2),(3,6),(2,5),(1,4),(4,7),(5,8),(6,9)], d="^")
+keys.add_edges_from([("A","3"),("0","2"),("3","6"),("2","5"),("1","4"),("4","7"),("5","8"),("6","9")], d="^")
 dirs.add_edges_from([(">","A"),("v","^")], d="^")
 #down
-keys.add_edges_from([(7,4),(8,5),(9,6),(4,1),(5,2),(6,3),(2,0),(3,"A")], d="v")
+keys.add_edges_from([("7","4"),("8","5"),("9","6"),("4","1"),("5","2"),("6","3"),("2","0"),("3","A")], d="v")
 dirs.add_edges_from([("A",">"),("^","v")], d="v")
 #left
-keys.add_edges_from([("A",0,),(3,2),(2,1),(6,5),(9,8),(8,7),(5,4)], d="<")
+keys.add_edges_from([("A","0",),("3","2"),("2","1"),("6","5"),("9","8"),("8","7"),("5","4")], d="<")
 dirs.add_edges_from([("A","^"),(">","v"),("v","<")], d="<")
 #right
-keys.add_edges_from([("0","A"),(2,3),(1,2),(5,6),(8,9),(7,8),(4,5)], d=">")
+keys.add_edges_from([("0","A"),("2","3"),("1","2"),("5","6"),("8","9"),("7","8"),("4","5")], d=">")
 dirs.add_edges_from([("^","A"),("v",">"),("<","v")], d=">")
 
-keymap={'0':[1,3],'A': [2,3],'1':[0,2],'2':[1,2],'3':[2,2],'4':[0,1],'5':[1,1],'6':[2,1],'7':[0,0],'8':[1,0],'9':[2,0]}
-dirmap={'^':[1,0],'A':[2,0],'<':[0,1],'v':[1,1],'>':[2,1]}
+keypaths=dict(nx.all_pairs_all_shortest_paths(keys))
+dirpaths=dict(nx.all_pairs_all_shortest_paths(dirs))
+
+for k,v in keypaths.items():
+    print(k,v)
+for k,v in dirpaths.items():
+    print(k,v)
+
+#print(keypaths["A"][7])
 
 codes=[]
 for line in f.readlines():
     codes.append(line.strip())
 
 total=0
-for i in codes:
-    print(i)
-    seq=mapkeypad(i)
-    print(seq)
-    seq2=mapdirpad(seq)
-    print(seq2)
-    seq3=mapdirpad(seq2)
-    print(seq3)
-    cost=int(i[0:-1])
-    print(len(seq3),cost)
-    cost=cost*len(seq3)
-    total+=cost
-    print(cost)
+for code in codes:
+    paths=set()
+    #for each key pair, return all paths, then recurse for each each of those until end of code
+    for i in range(len(code)-1):
+        print(code[i],code[i+1])
+        paths=getkeypaths(code[i],code[i+1],paths)
+    print(code)
+    for p in paths:
+        print(p)
 
 print(total)
 

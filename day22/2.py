ba135ea4cec2ef78171345478a269821c8aad5d0
Jag Bains <jagb@Jags-MBP.localnet>
2025-01-01 22:01:22 +0000

    part two

--- day22/2.py
+++ day22/2.py
@@ -32,12 +32,52 @@ for line in f.readlines():
     secrets.append(int(line.strip()))
 
 nsecrets=2000
+freq={}
 
-for i in range(nsecrets):
-    for i,secret in enumerate(secrets):
-        secrets[i]=gen_secret(secret)
+for i,secret in enumerate(secrets):
+    diff1=0
+    diff2=0
+    diff3=0
+    diff4=0
+    last=secret
+    for c in range(nsecrets):
+        diff4=diff3
+        diff3=diff2
+        diff2=diff1
+        s=gen_secret(last)
+        secrets[i]=s
+        diff1=int(str(s)[-1])-int(str(last)[-1])
+        last=s
+        if c>2:
+            if str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4) in freq:
+                if secret not in freq[str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4)]["secret"]:
+                    freq[str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4)]["secret"].append(secret)
+                    freq[str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4)]["total"]+=int(str(last)[-1])
+                    freq[str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4)]["count"]+=1
+            else:
+                freq[str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4)]={}
+                freq[str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4)]["secret"]=[secret]
+                freq[str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4)]["total"]=int(str(last)[-1])
+                freq[str(diff1)+"."+str(diff2)+"."+str(diff3)+"."+str(diff4)]["count"]=1
+
+
 total=0
 for secret in secrets:
     total+=secret
     print(secret)
 print("total:",total)
+
+mostfreq=0
+mostval=0
+mf=""
+mv=""
+for seq,value in freq.items():
+    if value["count"]>mostfreq:
+        mostfreq=value["count"]
+        mf=seq+":"+str(value["count"])+":"+str(value["total"])
+    if value["total"]>mostval:
+        mostval=value["total"]
+        mv=seq+":"+str(value["count"])+":"+str(value["total"])
+    print(seq, "count",value["count"],"total", value["total"])
+print("most frequent",mf)
+print("most valuable",mv)
